bb9fcc3d544245006384c5260b404ef652ab1915
GNdevdept <gndev.official@gmail.com>
2026-02-02 09:22:22 +0900

    Add files via upload

--- vote.js
+++ vote.js
@@ -5,27 +5,29 @@ function checkCode() {
   const msg = document.getElementById("codeMsg");
 
   if (input !== VALID_CODE) {
-    msg.innerText = "❌ 유효하지 않은 투표 코드입니다";
+    msg.innerText = "유효하지 않은 코드입니다";
     return;
   }
 
   if (localStorage.getItem(`voted_${VALID_CODE}`)) {
-    msg.innerText = "이미 이 투표에 참여하셨습니다";
+    msg.innerText = "이미 투표하셨습니다";
     return;
   }
 
   document.getElementById("code-screen").classList.add("hidden");
-  document.getElementById("vote-screen").classList.remove("hidden");
+  const vote = document.getElementById("vote-screen");
+  vote.classList.remove("hidden");
+  vote.classList.add("slide-up");
 }
 
-function vote(number) {
+function vote(num) {
   const msg = document.getElementById("voteMsg");
-  localStorage.setItem(`voted_${VALID_CODE}`, "true");
-  msg.style.color = "green";
-  msg.innerText = `${number}번에 투표 완료 🎉`;
+  localStorage.setItem(`voted_${VALID_CODE}`, true);
+  msg.innerText = `${num}번에 투표 완료! 🎉`;
+  msg.style.color = "#a7f3d0";
 
-  document.querySelectorAll(".options button").forEach(btn => {
-    btn.disabled = true;
-    btn.style.opacity = 0.5;
+  document.querySelectorAll(".grid button").forEach(b => {
+    b.disabled = true;
+    b.style.opacity = 0.5;
   });
 }
